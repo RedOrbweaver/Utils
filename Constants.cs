dd9b4e1766f9a73b16a214186b49ba96c9450e5c
developer <you@example.com>
2021-12-10 22:15:45 +0100

    User settings path

--- Constants.cs
+++ Constants.cs
@@ -22,6 +22,8 @@ public static class Constants
 
     public const string TILE_MODEL_DIRECTORY = "res://Models/Tiles";
 
+    public const string SETTINGS_PATH = "user://settings.json"; 
+
     public const float TILE_HEIGHT = 0.10f;
     public const float TILE_SIDE_LENGTH = 1.0f;
     public static readonly Godot.Vector2 TILE_SIZE = new Godot.Vector2(TILE_SIDE_LENGTH, TILE_SIDE_LENGTH);
